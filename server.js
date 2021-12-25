07fd59a00c2df266467de298812625ba37e28335
shaokat <shaokatcse@gmail.com>
2021-12-25 11:21:42 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const ejs = require("ejs")
+const path = require("path")
+const expressLayouts = require("express-ejs-layouts")
+const res = require("express/lib/response")
+
+
+const PORT = process.env.PORT || 4000
+
+app.use(express.static('public'))
+
+//set Template enjine
+
+app.use(expressLayouts)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+app.get('/', (req,res) => {
+    res.render('home')
+})
+
+app.get('/cart', (req,res) => {
+    res.render('customers/cart')
+})
+
+const  start = async () => {
+    app.listen(PORT, ()=>{
+        console.log(`app listing on port ${PORT}`)
+    })
+} 
+
+start()
